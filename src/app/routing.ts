cc9a9ab6012181c3410e6d20ba54919b317beef3
Ren Haojie <renhaojie520@live.cn>
2017-05-14 23:38:26 +0800

    save

---
+++ src/app/routing.ts
@@ -0,0 +1,6 @@
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+export const routing = RouterModule.forRoot([
+    { path: '', component: AppComponent }
+])
