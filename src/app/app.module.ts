cf2a54eb3c3cc29b03bc7371474cca807aaef391
renhaojie <renhaojie520@live.cn>
2017-05-15 17:13:07 +0800

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,24 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { routing } from './routing';
+import { CategoryComponent } from './category/category.component';
+import { UnitComponent } from './unit/unit.component';
+import { ProductComponent } from './product/product.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    UnitComponent,
+    ProductComponent,
+    ProductlistComponent,
+    OrderlistComponent,
+    UserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routing.ts
+++ src/app/routing.ts
@@ -1,6 +1,16 @@
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
+import { ProductComponent } from './product/product.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { UnitComponent } from './unit/unit.component';
 
 export const routing = RouterModule.forRoot([
-    { path: '', component: AppComponent }
-])
+    { path: '', component: OrderlistComponent },
+    { path: 'category', component: CategoryComponent },
+    { path: 'orderlist', component: OrderlistComponent },
+    { path: 'productlist', component: ProductlistComponent },
+    { path: 'productlist/:id', component: ProductComponent },
+    { path: 'unit', component: UnitComponent }
+]);
